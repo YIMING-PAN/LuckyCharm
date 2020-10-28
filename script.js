cc27edc66827e3afdb819f80b535927f73722c4d
Yiming Pan <panyimingau2017@gmail.com>
2020-10-28 11:06:09 +1000

    'update'

--- script.js
+++ script.js
@@ -1,49 +1,56 @@
-//set default degree (360*5)
-var degree = 1800;
-//number of clicks = 0
-var clicks = 0;
-
-var faces = [
-  "https://raw.githubusercontent.com/YIMING-PAN/LuckyCharm/master/images/face/face_1.png",
-  "https://raw.githubusercontent.com/YIMING-PAN/LuckyCharm/master/images/face/face_2.png",
+const faces = [
+  { src: "https://raw.githubusercontent.com/YIMING-PAN/LuckyCharm/images/face/caixukun.png", alt: "xukun" },
+  { src: "https://raw.githubusercontent.com/YIMING-PAN/LuckyCharm/images/face/guoqilin.png", alt: "qilin" },
+  { src: "https://raw.githubusercontent.com/YIMING-PAN/LuckyCharm/images/face/leijiayin.png", alt: "jiayin" },
+  { src: "https://raw.githubusercontent.com/YIMING-PAN/LuckyCharm/images/face/lisa.png", alt: "lisa" },
+  { src: "https://raw.githubusercontent.com/YIMING-PAN/LuckyCharm/images/face/shenteng.png", alt: "shenteng" },
+  { src: "https://raw.githubusercontent.com/YIMING-PAN/LuckyCharm/images/face/wangyibo.png", alt: "yibo" },
+  { src: "https://raw.githubusercontent.com/YIMING-PAN/LuckyCharm/images/face/wuyifan.png", alt: "yifan" },
+  { src: "https://raw.githubusercontent.com/YIMING-PAN/LuckyCharm/images/face/xiaozhan.png", alt: "xiaozhan" },
+  { src: "https://raw.githubusercontent.com/YIMING-PAN/LuckyCharm/images/face/yangchaoyue.png", alt: "chaoyue" },
+  { src: "https://raw.githubusercontent.com/YIMING-PAN/LuckyCharm/images/face/yiyangqianxi.png", alt: "qianxi" },
 ];
 
-$(document).ready(function () {
-  /*WHEEL SPIN FUNCTION*/
-  $("#start").click(function () {
-    //add 1 every click
-    clicks++;
+function handleStart() {
+  let timing = 12;
+  const startButton = document.getElementById("start");
+  startButton.disabled = true;
 
-    /*multiply the degree by number of clicks
-	  generate random number between 1 - 360, 
-    then add to the new degree*/
-    var newDegree = degree * clicks;
-    var extraDegree = Math.floor(Math.random() * 60) * 60;
-    totalDegree = newDegree + extraDegree;
+  if (startButton) {
+    beginSpin();
+    handleFaceChange();
+  }
 
-    /*let's make the spin btn to tilt every
-		time the edge of the section hits 
-		the indicator*/
-    $("#wheel .university").each(function () {
-      var t = $(this);
-      var noY = 0;
+  function beginSpin() {
+    const sheet = new CSSStyleSheet();
+    const degree = generateRandomRotateDeg();
+    sheet.insertRule(`#wheel {transform: rotate(${degree}deg)}`, 0);
+    document.adoptedStyleSheets = [sheet];
+  }
 
-      var c = 0;
-      var n = 550;
-      var interval = setInterval(function () {
-        c++;
-        if (c === n) {
-          clearInterval(interval);
-        }
-        //1.2、生成随机角标0~6
-        index = Math.floor(Math.random() * 2);
-        //1.3、设置小相框src属性
-        $("#face").prop("src", faces[index]);
-      }, 10);
+  function handleFaceChange() {
+    const loop = function () {
+      clearTimeout(self.timer);
+      const index = Math.floor(Math.random() * 10);
+      $("#face").prop("src", faces[index].src).prop("alt", faces[index].alt);
+      if (timing-- > 0) {
+        self.timer = setTimeout(loop, 500);
+      } else {
+        handleResult(faces[index].alt);
+        startButton.disabled = false;
+      }
+    }.bind(this);
+    loop();
+  }
+}
 
-      $("#wheel").css({
-        transform: "rotate(" + totalDegree + "deg)",
-      });
-    });
-  });
-}); //DOCUMENT READY
+function handleResult(result) {
+  console.log(result);
+}
+
+function generateRandomRotateDeg() {
+  const initialDegree = 720;
+  const randomDegree = Math.floor(Math.random() * 12) * 60;
+  const totalDegree = initialDegree + randomDegree;
+  return totalDegree;
+}
