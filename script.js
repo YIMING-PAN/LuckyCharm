f0389d0e1390057c6c0cc7435a0a48da2c11501a
Yiming Pan <panyimingau2017@gmail.com>
2020-10-28 11:10:34 +1000

    'update'

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 const faces = [
-  { src: "https://raw.githubusercontent.com/YIMING-PAN/LuckyCharm/images/face/caixukun.png", alt: "xukun" },
-  { src: "https://raw.githubusercontent.com/YIMING-PAN/LuckyCharm/images/face/guoqilin.png", alt: "qilin" },
-  { src: "https://raw.githubusercontent.com/YIMING-PAN/LuckyCharm/images/face/leijiayin.png", alt: "jiayin" },
-  { src: "https://raw.githubusercontent.com/YIMING-PAN/LuckyCharm/images/face/lisa.png", alt: "lisa" },
-  { src: "https://raw.githubusercontent.com/YIMING-PAN/LuckyCharm/images/face/shenteng.png", alt: "shenteng" },
-  { src: "https://raw.githubusercontent.com/YIMING-PAN/LuckyCharm/images/face/wangyibo.png", alt: "yibo" },
-  { src: "https://raw.githubusercontent.com/YIMING-PAN/LuckyCharm/images/face/wuyifan.png", alt: "yifan" },
-  { src: "https://raw.githubusercontent.com/YIMING-PAN/LuckyCharm/images/face/xiaozhan.png", alt: "xiaozhan" },
-  { src: "https://raw.githubusercontent.com/YIMING-PAN/LuckyCharm/images/face/yangchaoyue.png", alt: "chaoyue" },
-  { src: "https://raw.githubusercontent.com/YIMING-PAN/LuckyCharm/images/face/yiyangqianxi.png", alt: "qianxi" },
+  { src: "https://raw.githubusercontent.com/YIMING-PAN/LuckyCharm/master/images/face/caixukun.png", alt: "xukun" },
+  { src: "https://raw.githubusercontent.com/YIMING-PAN/LuckyCharm/master/images/face/guoqilin.png", alt: "qilin" },
+  { src: "https://raw.githubusercontent.com/YIMING-PAN/LuckyCharm/master/images/face/leijiayin.png", alt: "jiayin" },
+  { src: "https://raw.githubusercontent.com/YIMING-PAN/LuckyCharm/master/images/face/lisa.png", alt: "lisa" },
+  { src: "https://raw.githubusercontent.com/YIMING-PAN/LuckyCharm/master/images/face/shenteng.png", alt: "shenteng" },
+  { src: "https://raw.githubusercontent.com/YIMING-PAN/LuckyCharm/master/images/face/wangyibo.png", alt: "yibo" },
+  { src: "https://raw.githubusercontent.com/YIMING-PAN/LuckyCharm/master/images/face/wuyifan.png", alt: "yifan" },
+  { src: "https://raw.githubusercontent.com/YIMING-PAN/LuckyCharm/master/images/face/xiaozhan.png", alt: "xiaozhan" },
+  { src: "https://raw.githubusercontent.com/YIMING-PAN/LuckyCharm/master/images/face/yangchaoyue.png", alt: "chaoyue" },
+  { src: "https://raw.githubusercontent.com/YIMING-PAN/LuckyCharm/master/images/face/yiyangqianxi.png", alt: "qianxi" },
 ];
 
 function handleStart() {
